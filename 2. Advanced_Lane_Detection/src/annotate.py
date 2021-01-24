5ab24e91421f1a8e8d5546c493f88531f47fd58a
Bowen Chen <chenbowen184@gmail.com>
2021-01-24 09:34:13 -0800

    add main, refactor

--- 2. Advanced_Lane_Detection/src/annotate.py
+++ 2. Advanced_Lane_Detection/src/annotate.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class annotateFrame:
+class AnnotateFrame:
     """ Display road related information from detected lines and filtered image """
 
     def __init__(
